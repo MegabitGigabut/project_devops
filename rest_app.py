f830838d6981ed341fcfda19fbacaa06b3f907bb
MegabitGigabut <nurakloend77@gmail.com>
2023-12-13 17:56:28 +0100

    Commit Dev3

--- rest_app.py
+++ rest_app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 #Commit Dev 2
+#Commit Dev 3
 @app.route("/users/<user_id>", methods=["POST", "GET", "PUT", "DELETE"])
 def interact_with_use(user_id):
     if request.method == 'POST':
